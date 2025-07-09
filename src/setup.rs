c3c1d93e66d6c1af93e7521fd5086d9ff2aa2e00
Roger Padrell Casar <padrell.roger@gmail.com>
2025-07-09 21:06:08 +0200

    Change setup to it's own file

---
+++ src/setup.rs
@@ -0,0 +1,18 @@
+pub fn setup(){
+    // Config and plugin dirs
+    println!("- NOCAST setup");
+    
+    // clear existing setup
+    println!("Cleaning possible traces of other setups...");
+    if std::path::Path::new(&(bin_path() + "/nocast.toml")).is_file(){
+        fs::remove_file(bin_path() + "/nocast.toml").expect("Could not remove existing config file");
+    }
+    if std::path::Path::new(&(bin_path() + "/plugins/")).is_dir(){
+        fs::remove_dir_all(bin_path() + "/plugins/").expect("Could not remove existing plugins dir");
+    }
+    
+    // create new setup
+    println!("Creating new setup...");
+    fs::write(bin_path() + "/nocast.toml", "[plugins]").expect("Could not create config file");
+    fs::create_dir(bin_path() + "/plugins/").expect("Could not create plugins dir");
+}
