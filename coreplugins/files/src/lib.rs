d4c960396f9c3f87ce249b29e4bd39fb2dff8085
Roger Padrell Casar <padrell.roger@gmail.com>
2025-07-11 18:40:05 +0200

    Add copy_path to 'files' core plugin

--- coreplugins/files/src/lib.rs
+++ coreplugins/files/src/lib.rs
@@ -2,9 +2,10 @@ use nocast_plugincore::*;
 mod search;
 use crate::search::fast_search;
 use std::env;
+use arboard::Clipboard;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn sample(input: plugin_input) -> plugin_output {
+pub extern "C" fn search_files(input: plugin_input) -> plugin_output {
     let input_vec: Vec<String> = parse_input(input);
     
     if &input_vec[1] == ""{
@@ -18,8 +19,20 @@ pub extern "C" fn sample(input: plugin_input) -> plugin_output {
     
     let mut res: Vec<ActionOutput> = Vec::new();
     for path in results {
-        res.push(ActionOutput {name: path.clone(), description: "Copy path to clipboard".to_string(), target: ("copy,".to_owned() + &path).to_string()});
+        res.push(ActionOutput {name: path.clone(), description: "Copy path to clipboard".to_string(), target: ("copy_path,".to_owned() + &path).to_string()});
     }
 
+    return prepare_output(res);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn copy_path(input: plugin_input) -> plugin_output {
+	let input_vec: Vec<String> = parse_input(input);
+    
+    let mut clipboard = Clipboard::new().expect("Failed to initialize clipboard");
+    clipboard.set_text(input_vec[0].clone()).expect("Failed to set clipboard contents");
+    
+    let mut res: Vec<ActionOutput> = Vec::new();
+	res.push(ActionOutput {name: "{exit}".to_string(), description: String::new(), target: "exit,now".to_string()});
     return prepare_output(res);
 }
