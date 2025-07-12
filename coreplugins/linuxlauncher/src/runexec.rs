3fad77fbc53757e6cfec017b4993a863bfc36a17
Roger Padrell Casar <padrell.roger@gmail.com>
2025-07-12 20:50:54 +0200

    Finish separation

--- coreplugins/linuxlauncher/src/runexec.rs
+++
@@ -1,6 +0,0 @@
-pub fn run_exec(path: &str){
-    std::process::Command::new("sh")
-    	.arg("-c")
-   	 .arg(format!("nohup {} >/dev/null 2>&1 &", path))
-   	 .spawn().expect("Error spawning process");
-}
